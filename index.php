e65087921bf55ae6d9a0b7dfbd1f8833465ed94f
dj+1@programmr.com <dj+1@programmr.com>
2018-02-21 09:19:02 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -5,7 +5,14 @@ include_once 'dbconfig.php';
 if(isset($_GET['delete_id']))
 {
 	//delete logic here
+	//dj on 21 feb 
+	//this is my delete logic
+	$sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
 	
+	mysql_query($sql_query);
+	
+	header("Location: $_SERVER[PHP_SELF]");
+	//$$$ 
 }
 // delete condition
 
